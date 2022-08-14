1e3e7045b2a935342d004fc6e5399574828ae737
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-14 15:48:13 +0900

    모달연습, 하트 버튼 구현

--- js/honbabdetail.js
+++ js/honbabdetail.js
@@ -1,16 +1,41 @@
-const profileImg = document.querySelector(".detail_img")
+const open = document.querySelector(".detail_img")
 const popup = document.getElementById("popup")
 const body = document.querySelector("body")
+const close = document.querySelector(".profile_img")
+const blur = document.querySelector(".blur")
+const header = document.querySelector("header")
 
-
-function showProfile(){
-    const popupValue = popup.classList;
-    if(popupValue.contains("hidden")){
-        popup.classList.remove("hidden");
-    } else {
-        popup.classList.add("hidden");
-    }
-    
+function init(){
+    open.addEventListener("click", function(){
+        popup.classList.remove("hidden")
+        blur.classList.remove("hidden")
+        header.classList.add("hidden")
+    })
+    close.addEventListener("click",function(){
+        popup.classList.add("hidden")
+        blur.classList.add("hidden")
+        header.classList.remove("hidden")
+    })
 }
 
-profileImg.addEventListener("click",showProfile)
+init();
+// heart btn 
+
+const countHeart = document.querySelector("#heartBtn")
+
+
+let count = 0;
+let result = countHeart.innerText;
+function counter() {
+    if(count %2 == 0) {
+        countHeart.classList.add("btn_clicked");
+        result = parseInt(result)+1;
+        console.log(result)
+    } else {
+        countHeart.classList.remove("btn_clicked");
+        result = parseInt(result)-1;
+    }
+    count+=1;
+   
+}
+
