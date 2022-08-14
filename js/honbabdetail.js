49c50c868e15c9a2a52b3c4e288b9e03925dd7ac
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-14 20:24:01 +0900

    중간 저장

--- js/honbabdetail.js
+++ js/honbabdetail.js
@@ -1,24 +1,4 @@
-const open = document.querySelector(".detail_img")
-const popup = document.getElementById("popup")
-const body = document.querySelector("body")
-const close = document.querySelector(".profile_img")
-const blur = document.querySelector(".blur")
-const header = document.querySelector("header")
 
-function init(){
-    open.addEventListener("click", function(){
-        popup.classList.remove("hidden")
-        blur.classList.remove("hidden")
-        header.classList.add("hidden")
-    })
-    close.addEventListener("click",function(){
-        popup.classList.add("hidden")
-        blur.classList.add("hidden")
-        header.classList.remove("hidden")
-    })
-}
-
-init();
 // heart btn 
 
 const countHeart = document.querySelector("#heartBtn")
