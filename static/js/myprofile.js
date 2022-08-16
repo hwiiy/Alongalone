9eba789d54d62635dbcd274c4c2f73ea76d59033
2Sumin <lsmlsm426@knu.ac.kr>
2022-08-17 00:39:19 +0900

    마이프로필 수정

---
+++ static/js/myprofile.js
@@ -0,0 +1,37 @@
+// 포스트 내용 바뀌게
+
+// 혼밥
+function setInnerHTML() {
+  var x='<br></br>\n<hr style="width: 1050px; text-align: center; margin-top: 0;">\n<div class="posts">\n<div class="post_row">\n<div class="post green">1</div>\n<div class="post green">2</div>\n<div class="post green">3</div>\n</div>\n<br></br>\n<div class="post_row">\n<div class="post green">4</div>\n<div class="post green">5</div>\n<div class="post green">6</div>\n</div>\n</div>';
+  
+  const element = document.getElementById('category');
+  element.innerHTML 
+    =x;
+} 
+
+// 혼술
+function setInnerHTML2() {
+  var x='<br></br>\n<hr style="width: 1050px; text-align: center; margin-top: 0;">\n<div class="posts">\n<div class="post_row">\n<div class="post green">1</div>\n<div class="post green">2</div>\n<div class="post green">3</div>\n</div>\n<br></br>\n<div class="post_row">\n<div class="post green">4</div>\n<div class="post green">5</div>\n<div class="post green">6</div>\n</div>\n</div>';
+  
+  const element = document.getElementById('category');
+  element.innerHTML 
+    =x;
+} 
+
+// 혼카페
+function setInnerHTML3() {
+  var x='<br></br>\n<hr style="width: 1050px; text-align: center; margin-top: 0;">\n<div class="posts">\n<div class="post_row">\n<div class="post green">1</div>\n<div class="post green">2</div>\n<div class="post green">3</div>\n</div>\n<br></br>\n<div class="post_row">\n<div class="post green">4</div>\n<div class="post green">5</div>\n<div class="post green">6</div>\n</div>\n</div>';
+  
+  const element = document.getElementById('category');
+  element.innerHTML 
+    =x;
+} 
+
+// 전체보기
+function setInnerHTML4() {
+  var x='<br></br>\n<hr style="width: 1050px; text-align: center; margin-top: 0;">\n<div class="posts">\n<div class="post_row">\n<div class="post green">1</div>\n<div class="post green">2</div>\n<div class="post green">3</div>\n</div>\n<br></br>\n<div class="post_row">\n<div class="post green">4</div>\n<div class="post green">5</div>\n<div class="post green">6</div>\n</div>\n</div>';
+  
+  const element = document.getElementById('category');
+  element.innerHTML 
+    =x;
+} 
